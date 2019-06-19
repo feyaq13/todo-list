fc34897376c7f29db03bc01ddae6d002e8f39fbd
feyaq <feyaq13@gmail.com>
2019-06-20 00:03:52 +0300

    чистит синтаксис

--- src/main.js
+++ src/main.js
@@ -1,38 +1,23 @@
-const searchInputForTask = document.getElementsByClassName('input-group')[0];
-const inputForTask = searchInputForTask.getElementsByClassName('input-add-tasks')[0];
+const searchInputForTask = document.getElementsByClassName('input-group')[0]
+const inputForTask = searchInputForTask.getElementsByClassName('input-add-tasks')[0]
 
-const searchButtonAddTask = searchInputForTask.getElementsByClassName('input-group-append')[0];
-const buttonAddTask = searchButtonAddTask.getElementsByClassName('btn-add-task')[0];
+const searchButtonAddTask = searchInputForTask.getElementsByClassName('input-group-append')[0]
+const buttonAddTask = searchButtonAddTask.getElementsByClassName('btn-add-task')[0]
 
-const contain = document.getElementsByClassName('task-field')[0];
+const contain = document.getElementsByClassName('task-field')[0]
 
 const storedTodos = localStorage.todos
 const todos = storedTodos ? JSON.parse(storedTodos) : []
 
 for (const todo of todos) {
-
-  // addFragmentHtml(todo)
-  // const templateHtml = `
-  //   <div class="container task-field">
-  //     <div class="input-group mb-3">
-  //       <div class="input-group-prepend">
-  //         <div class="input-group-text">
-  //           <input type="checkbox" aria-label="Checkbox for following text input">
-  //         </div>
-  //       </div>
-  //       <input value="${todo}" type="text" class="form-control" aria-label="Text input with checkbox">
-  //     </div>
-  //   </div>
-  // `
-
   contain.innerHTML += addFragmentHtml(todo)
 }
 
 buttonAddTask.addEventListener('click', function () {
-  var todoName = String(inputForTask.value);
+  var todoName = String(inputForTask.value)
 
   if (!todoName) {
-    alert('Нельзя добавить пустую задачу');
+    alert('Нельзя добавить пустую задачу')
 
     return
   }
@@ -40,27 +25,12 @@ buttonAddTask.addEventListener('click', function () {
   todos.push(todoName)
   localStorage.todos = JSON.stringify(todos)
 
-  // const templateHtml = `
-  //   <div class="container task-field">
-  //     <div class="input-group mb-3">
-  //       <div class="input-group-prepend">
-  //         <div class="input-group-text">
-  //           <input type="checkbox" aria-label="Checkbox for following text input">
-  //         </div>
-  //       </div>
-  //       <input value="${todoName}" type="text" class="form-control" aria-label="Text input with checkbox">
-  //     </div>
-  //   </div>
-  // `
-
-  // contain.innerHTML += templateHtml
-  contain.innerHTML +=  addFragmentHtml(todoName)
+  contain.innerHTML += addFragmentHtml(todoName)
   inputForTask.value = null
 })
 
 function addFragmentHtml(name) {
   const templateHtml = `
-  <div class="container task-field">
     <div class="input-group mb-3">
       <div class="input-group-prepend">
         <div class="input-group-text">
@@ -69,7 +39,6 @@ function addFragmentHtml(name) {
       </div>
       <input value="${name}" type="text" class="form-control" aria-label="Text input with checkbox">
     </div>
-  </div>
 `
   return templateHtml
 }
