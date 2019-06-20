c975ffed055d94b804bef22cc88d0a969700b523
feyaq <feyaq13@gmail.com>
2019-06-20 15:41:53 +0300

    добавляет событие выбора и переноса завершённой задачи

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const inputForTask = searchInputForTask.getElementsByClassName('input-add-task')
 const searchButtonAddTask = searchInputForTask.getElementsByClassName('input-group-append')[0]
 const buttonAddTask = searchButtonAddTask.getElementsByClassName('btn-add-task')[0]
 
-const contain = document.getElementsByClassName('task-field')[0]
+const contain = document.getElementsByClassName('all-tasks-field')[0]
 
 const storedTodos = localStorage.todos
 const todos = storedTodos ? JSON.parse(storedTodos) : []
@@ -31,14 +31,29 @@ buttonAddTask.addEventListener('click', function () {
 
 function addFragmentHtml(name) {
   const templateHtml = `
-    <div class="input-group mb-3">
+    <div class="input-group mb-3 task">
       <div class="input-group-prepend">
         <div class="input-group-text">
-          <input type="checkbox" aria-label="Checkbox for following text input">
+          <input class="input-checkbox" type="checkbox" aria-label="Checkbox for following text input">
         </div>
       </div>
-      <input value="${name}" type="text" class="form-control" aria-label="Text input with checkbox">
+      <input value="${name}" type="text" class="form-control input-task_checked" aria-label="Text input with checkbox">
     </div>
 `
   return templateHtml
 }
+
+const taskDone = document.getElementsByClassName('task-done')[0]
+const inputCheckbox = Array.from(document.getElementsByClassName('input-group-prepend'))
+// const task = Array.from(document.getElementsByClassName('task'))
+const taskListDone = document.getElementsByClassName('task-list-done')[0]
+
+inputCheckbox.forEach(function (input) {
+  input.addEventListener('click', function (event) {
+    setTimeout(function () {
+      event.target.offsetParent.classList.add('task-hidden')
+    }, 900)
+    taskListDone.innerHTML = 'Завершённые задачи'
+    taskDone.innerHTML += event.target.offsetParent.outerHTML
+  })
+})
