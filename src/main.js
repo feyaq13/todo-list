1075cc40bf66c61bc007d89e2e1194f845612491
Semyon Fomin <taxnuke@gmail.com>
2019-06-27 14:57:38 +0300

    меняет кодстайл и рефакторит чутка

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-init()
+const model = restoreOrCreateModel();
+
+init();
 
 /**
  * Восстанавливает модель из localStorage или создает новую
@@ -11,7 +13,7 @@ function restoreOrCreateModel() {
 
   // восстанавливает модель
   if (localStorage.todosModel) {
-    return JSON.parse(localStorage.todosModel)
+    return JSON.parse(localStorage.todosModel);
   }
 
   // создаёт модель
@@ -20,46 +22,44 @@ function restoreOrCreateModel() {
     currentTodos: [],
     // перемещается по клику на таске, которая уже существует
     finishedTodos: []
-  }
+  };
 
-  return todosModel
+  return todosModel;
 }
 
-function addTodo(todoName, model) {
-  model = restoreOrCreateModel()
-  todoName = getTodoName()
+function addTodo(todoName) {
+  todoName = getTodoName();
 
   // TODO: сделать нормальную валидацию
   if (!todoName) {
-    alert('Нельзя добавить пустую задачу')
+    alert("Нельзя добавить пустую задачу");
 
-    return
+    return;
   }
 
-  model.currentTodos.push(todoName)
+  model.currentTodos.push(todoName);
 
-  clearFieldTask()
-
-  saveModel(model)
-  renderModel(model)
+  clearFieldTask();
+  saveModel(model);
+  renderModel(model);
 }
 
 // отдаёт значение поля таски
 function getTodoName() {
-  const inputForTask = document.getElementsByClassName('input-add-task')[0]
-  return inputForTask.value
+  const inputForTask = document.getElementsByClassName("input-add-task")[0];
+  return inputForTask.value;
 }
 
 // очищает поле ввода таски
 function clearFieldTask() {
-  document.getElementsByClassName('input-add-task')[0].value = null
+  document.getElementsByClassName("input-add-task")[0].value = null;
 }
 
 /**
  * Сохраняет модель в localStorage
  */
 function saveModel(model) {
-  localStorage.todosModel = JSON.stringify(model)
+  localStorage.todosModel = JSON.stringify(model);
 }
 
 /**
@@ -67,63 +67,40 @@ function saveModel(model) {
  */
 // TODO: проверить количество отрисовок
 function renderModel(model) {
-
-  model = restoreOrCreateModel()
-
-  const tasksContainer = document.getElementsByClassName('all-tasks-field')[0]
-
-  tasksContainer.innerHTML = ''
+  const tasksContainer = document.getElementsByClassName("all-tasks-field")[0];
+  tasksContainer.innerHTML = "";
 
   for (const todo of model.currentTodos) {
-    tasksContainer.innerHTML += addFragmentHtml(todo)
+    tasksContainer.innerHTML += generateTodoHtml(todo);
   }
 
-  // TODO: сделать шаблонизацию
-  function addFragmentHtml(name) {
-    const templateHtml = `
-      <div class="input-group mb-3 task">
-        <div class="input-group-prepend">
-          <div class="input-group-text">
-            <input class="input-checkbox" type="checkbox" aria-label="Checkbox for following text input">
-          </div>
-        </div>
-        <input value="${name}" type="text" class="form-control input-task_checked" aria-label="Text input with checkbox">
-      </div>
-  `
-    return templateHtml
-  }
-}
+  const tasksListDone = document.getElementsByClassName("tasks-list-done")[0];
+  const tasksDone = document.getElementsByClassName("tasks-done")[0];
 
-function renderDoneTodo(model) {
-
-  model = restoreOrCreateModel()
-
-  const tasksListDone = document.getElementsByClassName('tasks-list-done')[0]
-  const tasksDone = document.getElementsByClassName('tasks-done')[0]
-
-  tasksDone.innerHTML = ''
+  tasksDone.innerHTML = "";
 
   if (model.finishedTodos.length > 0) {
-    tasksListDone.textContent = 'Завершённые задачи'
+    tasksListDone.textContent = "Завершённые задачи";
   }
 
   for (const todo of model.finishedTodos) {
-    tasksDone.innerHTML += addFragmentHtml(todo)
+    tasksDone.innerHTML += generateTodoHtml(todo, true);
   }
 
-  // TODO: сделать шаблонизацию
-  function addFragmentHtml(name) {
+  function generateTodoHtml(name, isDone) {
     const templateHtml = `
       <div class="input-group mb-3 task">
         <div class="input-group-prepend">
           <div class="input-group-text">
-            <input class="input-checkbox" type="checkbox" checked aria-label="Checkbox for following text input">
+            <input class="input-checkbox" type="checkbox" ${
+              isDone ? "checked" : ""
+            } aria-label="Checkbox for following text input">
           </div>
         </div>
         <input value="${name}" type="text" class="form-control input-task_checked" aria-label="Text input with checkbox">
       </div>
-  `
-    return templateHtml
+  `;
+    return templateHtml;
   }
 }
 
@@ -132,63 +109,66 @@ function renderDoneTodo(model) {
  * (добавляет обработчики и тд)
  */
 function init() {
+  const inputForTask = document.getElementsByClassName("input-add-task")[0];
 
-  const inputForTask = document.getElementsByClassName('input-add-task')[0]
-
-  // добавляет карточку по нажатию на ENTER
-  inputForTask.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-      addTodo()
-    }
-  })
+  inputForTask.addEventListener("keydown", enterHandler);
 
   // добавляет карточку по нажатию кнопки "Добавить"
-  const buttonAddTask = document.getElementsByClassName('btn-add-task')[0]
+  const buttonAddTask = document.getElementsByClassName("btn-add-task")[0];
+  buttonAddTask.addEventListener("click", addTodo);
 
-  buttonAddTask.addEventListener('click', addTodo)
-  //
+  const tasksContainer = document.getElementsByClassName("all-tasks-field")[0];
+  tasksContainer.addEventListener("click", checkTarget);
 
-  // управляет задачами помеченными как "завершенные"
-  const tasksDoneBlock = document.getElementsByClassName('tasks-done')[0]
-  const tasksContainer = document.getElementsByClassName('all-tasks-field')[0]
+  renderModel(model);
 
-
-  tasksContainer.addEventListener('click', function (event) {
+  function checkTarget(event) {
     // проверяем на что нажимает пользователь: при попадании мимо нужного блока, просто будет выход из функции
     if (event.target === tasksContainer) {
-      return
+      return;
     }
 
-    checkedTask(event)
+    checkedTask(event);
 
-    const element = $(event.target.offsetParent).slideToggle('fast', function () {
+    const element = $(event.target.offsetParent).slideToggle(
+      "fast",
+      function() {
+        toggleTodoStatus(element, model);
+        element.slideToggle();
+      }
+    );
+  }
 
-      // doneTodo(element, model)
-      tasksDoneBlock.appendChild(element.get(0))
-      element.slideToggle()
-    })
-
-  })
-
-  const model = restoreOrCreateModel()
-  renderModel(model)
-  renderDoneTodo(model)
+  function enterHandler(e) {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  }
 }
 
 // TODO: сделать функцию-проверку таски, на наличие метки checked внезависимости от того, находится ли она в currentTodos или в finishedTodos
-function checkedTask(event) { // toggleCheckTask()
-  event.target.offsetParent.getElementsByClassName('input-checkbox')[0].checked = true
+function checkedTask(event) {
+  // toggleCheckTask()
+  event.target.offsetParent.getElementsByClassName(
+    "input-checkbox"
+  )[0].checked = true;
 }
 
-function doneTodo(element, model) {
-  model = restoreOrCreateModel()
+function toggleTodoStatus(element) {
+  const triggeredElement = element[0].getElementsByClassName(
+    "input-task_checked"
+  )[0];
+  const taskName = triggeredElement.value;
+  const isDone = model.finishedTodos.includes(taskName);
 
-  const doneTask = element[0].getElementsByClassName('input-task_checked')[0].value
+  if (isDone) {
+    model.finishedTodos.splice(model.finishedTodos.indexOf(taskName), 1);
+    model.currentTodos.push(taskName);
+  } else {
+    model.currentTodos.splice(model.currentTodos.indexOf(taskName), 1);
+    model.finishedTodos.push(taskName);
+  }
 
-  model.currentTodos.splice(model.currentTodos.indexOf(doneTask), 1)
-  model.finishedTodos.push(doneTask)
-
-  saveModel(model)
-  renderModel(model)
-  renderDoneTodo(model)
+  saveModel(model);
+  renderModel(model);
 }
