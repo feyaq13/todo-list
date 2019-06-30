2a22c38713dffc7eeedcf3eac121aa2e59eaef8b
Semyon Fomin <taxnuke@gmail.com>
2019-06-30 12:35:24 +0300

    добавляет eslint скрипт и переносит browser-sync в зависимости для разработки

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jquery": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    },
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    semi: [2, "always"]
+  }
 };
